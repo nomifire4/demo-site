37e38ab83f5610bdfc5eda844ee20aa6ab8d6dd5
Louis Cundari III <louiscundari3@outlook.com>
2024-11-11 16:28:34 -0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from 'next/link'
+// import '../styles/globals.css';
+
+
+import { mainModule } from "process";
+
+export default function Home() {
+  return (
+    /* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"> */
+    <main>
+      <h1 style={{textAlign: "center"}}>Hello World!</h1>
+      <Link href="/about">About Page Link</Link>
+      <button style={{ display: "block", margin: "20px 0" }}>
+         About Page Button
+      </button>
+      <button className="button">Click Me</button>
+      <div className="image-grid">
+        <Image src="/images/image.png" alt="Image" width={200} height={200} />
+        <Image src="/images/image.png" alt="Image" width={200} height={200} />
+        <Image src="/images/image.png" alt="Image" width={200} height={200} />
+        <Image src="/images/image.png" alt="Image" width={200} height={200} />
+        <Image src="/images/image.png" alt="Image" width={200} height={200} />
+        <Image src="/images/image.png" alt="Image" width={200} height={200} />
+      </div>
+
+    </main>
+      
+      
+      
+  );
+}
